0e1a7bf20cfc7eec9f5e8cfd9c39497464606606
Josh Brandl <brandljoshua@gmail.com>
2018-02-27 14:15:56 -0500

    Added router

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  //Login page?
+  { path: 'login', component: AppComponent},
+  { path: 'home', redirectTo: '/login',   data: { title: 'Heroes List' } },
+  
+  //Might want to change this to an error page?
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
