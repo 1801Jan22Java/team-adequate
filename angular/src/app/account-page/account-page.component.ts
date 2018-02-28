3e886b52a4c3f5351fce05d6110d9c202f918297
Josh Brandl <brandljoshua@gmail.com>
2018-02-28 16:48:59 -0500

    More work on profile page

--- angular/src/app/account-page/account-page.component.ts
+++ angular/src/app/account-page/account-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user'
 
 @Component({
   selector: 'app-account-page',
@@ -11,5 +12,15 @@ export class AccountPageComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  //Placeholder, load this from the backend
+  user : User = {
+    id : 0,
+
+    username : "username",
+    firstname : "firstname",
+    lastname : "lastname",
+    description : "This is a test description for the user"
+  };
 
 }
