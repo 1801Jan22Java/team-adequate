1f35469d5ffecaa0c5ed9ebab15fd6d7ac20c137
Michael Chen <mchen092@gmail.com>
2018-02-28 23:36:54 -0500

    made search pretty

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -12,8 +12,32 @@ export class SearchComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  togglePeople() : void {
+      this.searchPeople = !this.searchPeople
+  }
+
+  changePriceString() : void {
+    this.searchPriceString = '';
+    for(var i = this.searchPrice; i >= 1; i--) {
+      this.searchPriceString += '$';
+    }
+  }
+
+  changeRatingString() : void {
+    this.searchRatingString = 'Rating:';
+    for(var i = this.searchRating; i >= 1; i--) {
+      this.searchRatingString += ' &#x272F;';
+    }
+  }
+
+
   //Search params
+  searchPeople : boolean = true;
   searchDistance : number = 10;
   searchPrice : number = 1;
   searchRating : number = 1;
+
+  searchPriceString : string = '$';
+  searchRatingString :string =  'Rating: &#x272F;';
 }
