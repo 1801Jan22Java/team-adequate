46cece5572ef83b272dc09237b4ca3abf10c7bac
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-05 09:41:22 -0500

    Added logout controller logic

--- AdequateTravel/src/main/java/com/adequate/controller/LogoutController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/LogoutController.java
@@ -1,8 +1,18 @@
 package com.adequate.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import com.adequate.util.CurrentUser;
+
+@Controller("logoutController")
+@RequestMapping("/logout")
 public class LogoutController {
 
+	
+	public ModelAndView logout(){
+		CurrentUser.logout(); // null-ifies all info on user 
+		return new ModelAndView("redirect:/login");
+	}
 }
