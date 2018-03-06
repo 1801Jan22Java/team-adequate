4770a3eb5a843fd67273d012c3e685129a99755f
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-06 14:13:42 -0500

    Added ResponseBody to get request method

--- AdequateTravel/src/main/java/com/adequate/controller/ValidSessionController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/ValidSessionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.adequate.util.CurrentUser;
 
@@ -12,7 +13,9 @@ import com.adequate.util.CurrentUser;
 @RequestMapping("/validate")
 public class ValidSessionController {
 	
+	// Return a simple JSON string to tell client if session is valid
 	@RequestMapping(method=RequestMethod.GET)
+	@ResponseBody
 	public ResponseEntity<String> validateSession(){
 		if(CurrentUser.getValidSession())
 			return new ResponseEntity<>("{\"status\":\"success\"}", HttpStatus.ACCEPTED);
