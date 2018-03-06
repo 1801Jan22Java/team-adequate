8c9fbd829f848189ee10e6c4ad43a4530945cf51
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-06 15:34:52 -0500

    Added Itinerary Service/Controller stuff

--- AdequateTravel/src/main/java/com/adequate/controller/LogoutController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/LogoutController.java
@@ -1,12 +1,11 @@
 package com.adequate.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus; 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.adequate.util.CurrentUser;
 
--- AdequateTravel/src/main/java/com/adequate/controller/ReviewController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/ReviewController.java
@@ -1,8 +1,12 @@
 package com.adequate.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller("reviewController")
+@RequestMapping("/review")
 public class ReviewController {
 	
 	
