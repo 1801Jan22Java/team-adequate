1b14de936f52be052f00814fd9d41456c09f0abe
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-06 19:56:55 -0500

    Added methods to SearchController & commented out main methods

--- AdequateTravel/src/main/java/com/adequate/api/AutocompletePlaceGetter.java
+++ AdequateTravel/src/main/java/com/adequate/api/AutocompletePlaceGetter.java
@@ -4,13 +4,14 @@ import org.springframework.web.client.RestTemplate;
 
 public class AutocompletePlaceGetter {
 	
-	
+	/*
 	public static void main(String [] args) {
 		
 		AutocompletePlaceTemplate r = getAutocompletePlaceTemplate("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=paris&types=geocode&&key=AIzaSyA-cqeJ3xy8IXWiIffplTwOUqZODVvMBps");
 		System.out.println(r.toString());
 		
 	}
+	*/
 	
 	
 	public static AutocompletePlaceTemplate getAutocompletePlaceTemplate(String url) {
--- AdequateTravel/src/main/java/com/adequate/controller/SearchController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/SearchController.java
@@ -1,6 +1,6 @@
 package com.adequate.controller;
-import com.adequate.api.*;
 
+import com.adequate.api.*; 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.adequate.api.AutocompletePlaceTemplate;
-
 
 @CrossOrigin(origins = "http://localhost:4200")
 @Controller("searchController")
 @RequestMapping("/search")
 public class SearchController {
-	private final String apiKey = "&key=AIzaSyA4k2FBjButfxabh_rgO6DbK7_LSBQN538";
+	private final String apiKey = "&key=AIzaSyA-cqeJ3xy8IXWiIffplTwOUqZODVvMBps";
 	
 	// place search has extra parameters we care about
 	private String placeSearchPrefix = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
@@ -27,9 +25,32 @@ public class SearchController {
 	@RequestMapping(value="/autoComplete", method=RequestMethod.GET)
 	public ResponseEntity<AutocompletePlaceTemplate> autoComplete(@RequestParam("query") String query){
 		String url = autocompletePlacePrefix + query + apiKey;
-		
 		AutocompletePlaceTemplate autoCompletePlaceTemplate = AutocompletePlaceGetter.getAutocompletePlaceTemplate(url);
 		return new ResponseEntity<>(autoCompletePlaceTemplate,HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/placeDetailsSearch", method=RequestMethod.GET)
+	public ResponseEntity<PlaceDetailsTemplate> placeDetails(@RequestParam("placeID") String placeID){
+		String url = placeDetailsPrefix + placeID + apiKey;
+		PlaceDetailsTemplate placeDetailsTemplate = PlaceDetailsGetter.getPlaceDetailsTemplate(url);
+		return new ResponseEntity<>(placeDetailsTemplate,HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/placeSearch", method=RequestMethod.GET)
+	public ResponseEntity<PlaceSearchTemplate> placeSearch(@RequestParam("query") String query, 
+			@RequestParam("distance") Integer distance, @RequestParam("price") Integer price){
+		Integer meterDistance = 0;
+		
+		if(distance < 30)
+			meterDistance = 1600 * distance; // conversion between miles and meters
+		else
+			meterDistance = 49990; // Places api has max radius of 50 000 meters
+		
+		String parameters = query+"&radius="+meterDistance+"&maxPrice="+price;
+		
+		String url = placeSearchPrefix + parameters + apiKey;
+		PlaceSearchTemplate placeSearchTemplate = PlaceSearchGetter.getPlaceSearchTemplate(url);
+		return new ResponseEntity<>(placeSearchTemplate,HttpStatus.CREATED);
 	}
 	
 }
