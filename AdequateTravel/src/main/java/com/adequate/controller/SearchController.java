a47419347e0ca3d245be98632add3a93d92ade1f
Michael Chen <mchen092@gmail.com>
2018-03-06 19:16:26 -0500

    passing off to leonard the controller

--- AdequateTravel/src/main/java/com/adequate/controller/SearchController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/SearchController.java
@@ -1,19 +1,35 @@
 package com.adequate.controller;
+import com.adequate.api.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.adequate.api.AutocompletePlaceTemplate;
 
 
 @CrossOrigin(origins = "http://localhost:4200")
 @Controller("searchController")
 @RequestMapping("/search")
 public class SearchController {
+	private final String apiKey = "&key=AIzaSyA4k2FBjButfxabh_rgO6DbK7_LSBQN538";
 	
-	private final String geoCodingPrefix = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-	private final String geoCodingSuffix = "&key=AIzaSyA4k2FBjButfxabh_rgO6DbK7_LSBQN538";
+	// place search has extra parameters we care about
+	private String placeSearchPrefix = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+	private String placeDetailsPrefix = "https://maps.googleapis.com/maps/api/place/details/json?placeid=";
+	// add type=geocode afterwards input
+	private String autocompletePlacePrefix = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=";
 	
-	private final String placePrefix = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
-	private final String placeSuffix = "&key=AIzaSyA-cqeJ3xy8IXWiIffplTwOUqZODVvMBps";
+	@RequestMapping(value="/autoComplete", method=RequestMethod.GET)
+	public ResponseEntity<AutocompletePlaceTemplate> autoComplete(@RequestParam("query") String query){
+		String url = autocompletePlacePrefix + query + apiKey;
+		
+		AutocompletePlaceTemplate autoCompletePlaceTemplate = AutocompletePlaceGetter.getAutocompletePlaceTemplate(url);
+		return new ResponseEntity<>(autoCompletePlaceTemplate,HttpStatus.CREATED);
+	}
 	
 }
