372f64ae8256290357e8ca66a65738d91eac2ab3
Leonard Blumberg <tlblumberg@gmail.com>
2018-03-07 20:28:49 -0500

    Adding first test class

--- AdequateTravel/src/main/java/com/adequate/controller/ProfileController.java
+++ AdequateTravel/src/main/java/com/adequate/controller/ProfileController.java
@@ -96,7 +96,7 @@ public class ProfileController {
 			comparePerson.setFirstname(person.getFname());
 			comparePerson.setLastname(person.getLname());
 			comparePerson.setAbout(person.getDesc());
-			/*
+			
 			try {
 				byte [] image = person.getImg().getBytes(1l, (int) person.getImg().length());
 				comparePerson.setImage(image);
@@ -104,12 +104,6 @@ public class ProfileController {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-<<<<<<< HEAD
-=======
-			*/
-			//comparePerson.setImage(person.getImg());
-			// gonna figure out how to convert blob to byte[]
->>>>>>> b04ca05970a5794ed16d99334b6831376675166c
 		}
 		
 		return new ResponseEntity<>("{\"status\":\"success\"}", HttpStatus.ACCEPTED);
